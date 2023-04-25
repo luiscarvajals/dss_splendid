5b10e95df1f21ea27a5e90239271b35f4000b62f
luiscarvajals <luiscarvajalsanchez2@gmail.com>
2023-04-24 21:09:09 -0400

    inicio basico

--- index.php
+++ index.php
@@ -5,10 +5,33 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Inicio</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
-    <script src='js/main.js'></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+    <script src='js/index.js'></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-
+    <nav>
+        <img src="images/logo.jpeg" alt="logo">
+        <ul>
+            <li><a href="index.php">INICIO</a></li>
+            <li><a href="#">VENTAS</a></li>
+            <li><a href="#">PERSONAL</a></li>
+            <li><a href="#">INVENTARIO</a></li>
+            <li><a href="#">SALIR</a></li>
+        </ul>
+    </nav>
+    <img class="portada" src="images/cover.jpg" alt="hero">
+    <div class="contenedor">
+        <h1 class="bienvenida">BIENVENIDO DE VUELTA</h1>
+        <div class="recomendacion">
+            <h2>Esta es la recomendacion para el dia de hoy:</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates iusto temporibus omnis est consequuntur enim ad dolore nam, hic reiciendis sed consequatur, inventore quaerat. Debitis eos consectetur culpa temporibus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt esse totam ut, corrupti, eos rerum cumque necessitatibus veniam eum ducimus vel quam rem nobis, iste facere temporibus doloribus? Culpa, tempore.</p>
+        </div>
+    </div>
+    <footer>
+        <p>2023 por: Universidad Católica Boliviana "San Pablo"</p>
+    </footer>
 </body>
 </html>

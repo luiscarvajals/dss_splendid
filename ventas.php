53d1647e7ce2453edfb6bec0832d83e8cb6ff726
luiscarvajals <luiscarvajalsanchez2@gmail.com>
2023-04-24 21:23:45 -0400

    archivos base ventas

--- ventas.php
+++ ventas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Inicio</title>
+    <title>Ventas</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
     <script src='js/index.js'></script>
@@ -22,13 +22,8 @@
             <li><a href="#">SALIR</a></li>
         </ul>
     </nav>
-    <img class="portada" src="images/cover.jpg" alt="hero">
     <div class="contenedor">
-        <h1 class="bienvenida">BIENVENIDO DE VUELTA</h1>
-        <div class="recomendacion">
-            <h2>Esta es la recomendacion para el dia de hoy:</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates iusto temporibus omnis est consequuntur enim ad dolore nam, hic reiciendis sed consequatur, inventore quaerat. Debitis eos consectetur culpa temporibus libero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt esse totam ut, corrupti, eos rerum cumque necessitatibus veniam eum ducimus vel quam rem nobis, iste facere temporibus doloribus? Culpa, tempore.</p>
-        </div>
+
     </div>
     <footer>
         <p>2023 por: Universidad Católica Boliviana "San Pablo"</p>

bf7f8dd0b3e4229139f552d22ec17eb99e5fdf20
MARCK-J <105019521+MARCK-J@users.noreply.github.com>
2023-05-01 20:01:08 -0400

    C1

---
+++ bdd.php
@@ -0,0 +1,16 @@
+<?php
+    $server='localhost';
+    $username='root';
+    $password='';
+    $database  ='splendid';
+    
+$conexion = new mysqli($server, $username, $password, $database);
+
+if($conexion -> connect_errno) {
+  die("Conexio fallida". $conexion -> connect_errno );
+}else{
+  echo "Conectado";
+}
+
+
+?>

f3a3e0bd0cd60390641a5d6c7c6135f0008fbb8f
EdelyTito <104478773+EdelyTito@users.noreply.github.com>
2023-05-20 14:56:00 -0400

    ejemplo

--- empleados.php
+++ empleados.php
@@ -8,7 +8,7 @@ $dbname = "tienda_helados";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Error al conectar a la base de datos: " . $conn->connect_error);
+    die("Error al conectar a la base de datos helados: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM empleados";

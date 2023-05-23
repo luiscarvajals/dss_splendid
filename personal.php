c44390ed98792927e5526927441b580d7aed29ab
EdelyTito <104478773+EdelyTito@users.noreply.github.com>
2023-05-22 22:49:05 -0400

    personal

--- personal.php
+++ personal.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tienda_helados";
+$dbname = "splendid";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -44,66 +44,241 @@ $conn->close();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <style>
+        .chart-section {
+            margin-top: 45px;
+        }
+    </style>
+
 
     <nav>
         <a href="index.php"><img src="images/logo.jpeg" alt="logo"></a>
         <ul>
-            <li><a href="index.php">INICIO</a></li>
+            <li><a href="inicio.php">INICIO</a></li>
             <li><a href="ventas.php">VENTAS</a></li>
             <li><a href="personal.php">PERSONAL</a></li>
-            <li><a href="#">INVENTARIO</a></li>
-            <li><a href="#">SALIR</a></li>
+            <li><a href="dashboard.php">DASHBOARD</a></li>
+            <li><a href="inventario.php">INVENTARIO</a></li>
         </ul>
     </nav>
 </head>
 
 <body>
     <div class="container">
-        <header>
-            <h1>Personal de la Tienda de Helados</h1>
+        <header class="py-5 bg-secondary text-white text-center">
+            <h1 class="display-4">Personal de la Tienda SPLENDID</h1>
         </header>
 
-        <br><br>
-        <div class="container">
+        <div class="container py-5">
             <div class="row">
                 <?php foreach ($empleados as $empleado): ?>
-                    <div class="col-md-6 col-lg-3" data-aos="fade-up">
-                        <div class="card">
-                            <img src="<?php echo $empleado['imagen']; ?>" alt="Foto de <?php echo $empleado['nombre']; ?>">
-                            <div class="info">
-                                <h3>
-                                    <?php echo "Nombre completo: " . $empleado['nombre'] . " " . $empleado['apellidoP'] . " " . $empleado['apellidoM']; ?>
-                                </h3>
-                                <p>
-                                    <?php echo "Email:"." ".$empleado['email']; ?>
-                                </p>
-                                <p>
-                                    <?php echo "Puesto:"." ". $empleado['puesto']; ?>
-                                </p>
-                                <p>
-                                    <?php echo "Direccion:"." ". $empleado['direccion']; ?>
+                    <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?php echo $empleado['nombre'] . " " . $empleado['apellido']; ?>
+                                </h5>
+                                <p class="card-text">
+                                    <strong>Fecha de nacimiento:</strong>
+                                    <?php echo $empleado['fecha_nacimiento']; ?><br>
+                                    <strong>Dirección:</strong>
+                                    <?php echo $empleado['direccion']; ?><br>
+                                    <strong>Teléfono:</strong>
+                                    <?php echo $empleado['telefono']; ?><br>
+                                    <strong>Email:</strong>
+                                    <?php echo $empleado['email']; ?><br>
+                                    <strong>Puesto:</strong>
+                                    <?php echo $empleado['cargo']; ?><br>
+                                    <strong>Sueldo:</strong>
+                                    <?php echo $empleado['sueldo']; ?><br>
+                                    <strong>Fecha de contratación:</strong>
+                                    <?php echo $empleado['fecha_contratacion']; ?><br>
+                                    <strong>Código:</strong>
+                                    <?php echo $empleado['id']; ?>
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <br><br>
                 <?php endforeach; ?>
-                
+            </div>
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between">
+                    <a href="agregar_empleado.php" class="btn btn-primary">Agregar Empleado</a>
+                </div>
             </div>
         </div>
-        <br><br>
-        <a href="agregar_empleado.php" class="btn btn-primary">Agregar Empleado</a>
-        <a href="eliminar_empleado.php" class="btn btn-primary">Eliminar Empleado</a>
-        <a href="modificar_empleado.php" class="btn btn-primary">Modificar Empleado</a>
-        <br>
+
+        <!-- Gráfico de empleados por sucursal -->
+        <div class="chart-section">
+            <h2>Empleados por Sucursal</h2>
+            <div class="chart-container">
+                <canvas id="empleadosBySucursalChart" width="400" height="200"></canvas>
+            </div>
+        </div>
+
+
+        <?php
+        // Conexión a la base de datos
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "splendid";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Error de conexión: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT s.nombre AS sucursal, COUNT(ve.id_empleado) AS cantidad_empleados
+            FROM sucursales s
+            LEFT JOIN ventas_empleados ve ON s.id = ve.id_empleado
+            GROUP BY s.nombre";
+
+        $result = $conn->query($sql);
+
+        // Crear un array con los datos de empleados por sucursal
+        $labels = [];
+        $data = [];
+
+        while ($row = $result->fetch_assoc()) {
+            $labels[] = $row["sucursal"];
+            $data[] = $row["cantidad_empleados"];
+        }
+
+        ?>
+
+        <script>
+            // Obtener el contexto del lienzo del gráfico
+            var ctx = document.getElementById('empleadosBySucursalChart').getContext('2d');
+
+            // Datos del gráfico de empleados por sucursal
+            var empleadosBySucursalData = {
+                labels: <?php echo json_encode($labels); ?>,
+                datasets: [{
+                    data: <?php echo json_encode($data); ?>,
+                    backgroundColor: [
+                        '#FF6384',
+                        '#36A2EB',
+                        '#FFCE56',
+                        '#4BC0C0',
+                        '#9966FF'
+                    ],
+                    hoverBackgroundColor: [
+                        '#FF6384',
+                        '#36A2EB',
+                        '#FFCE56',
+                        '#4BC0C0',
+                        '#9966FF'
+                    ]
+                }]
+            };
+
+            // Configuración del gráfico
+            var chartOptions = {
+                responsive: true,
+                tooltips: {
+                    callbacks: {
+                        label: function (tooltipItem, data) {
+                            var dataset = data.datasets[tooltipItem.datasetIndex];
+                            var total = dataset.data.reduce(function (previousValue, currentValue) {
+                                return previousValue + currentValue;
+                            });
+                            var currentValue = dataset.data[tooltipItem.index];
+                            var percentage = Math.floor((currentValue / total) * 100 + 0.5);
+                            return percentage + "%";
+                        }
+                    }
+                }
+            };
+
+            // Crear el gráfico de barras de empleados por sucursal
+            var empleadosBySucursalChart = new Chart(ctx, {
+                type: 'bar',
+                data: empleadosBySucursalData,
+                options: chartOptions
+            });
+        </script>
+
+        =======
+        <table>
+            <thead>
+                <tr>
+                    <th>NOMBRE</th>
+                    <th>ROL</th>
+                    <th>HORARIO</th>
+                    <th>RENDIMIENTO</th>
+                    <th>SUCURSAL</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $empleados = mysqli_query($conn, "SELECT a.nombre, a.direccion, CONCAT(b.hora_entrada, ' - ', b.hora_salida) AS horario, a.telefono
+        FROM empleados a, horarios b
+        WHERE a.id = b.id_empleado");
+                while ($row = mysqli_fetch_assoc($empleados)) { ?>
+                    <tr>
+                        <td>
+                            <?php echo $row["nombre"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $row["direccion"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $row["horario"]; ?>
+                        </td>
+                        <td>
+                            <?php echo $row["telefono"]; ?>
+                        </td>
+                    </tr>
+                <?php }
+                mysqli_free_result($empleados); ?>
+            </tbody>
+        </table>
 
         <footer>
-            <p>2023 por: Universidad Católica Boliviana "San Pablo"</p>
+            <div class="footer-contenedor">
+                <div class="footer-info">
+                    <div class="footer-contact">
+                        <div class="infoimg">
+                            <img src="images/whatsapp.png" alt="hero">
+                            <p>TELEFONO: </p>
+                        </div>
+
+                        <p class="p1">271825944 </p>
+                    </div>
+
+                    <div class="footer-contact">
+                        <div class="infoimg">
+                            <img src="images/sucursal.png" alt="hero">
+                            <p>SUCURSALES: </p>
+                        </div>
+
+                        <p class="p1">Zona San Pedro Calle, Nicolás Acosta N°420</p>
+                        <p class="p1">Zona Sur, Calle 15 de Calacoto</p>
+                    </div>
+
+                    <div class="footer-contact">
+                        <div class="infoimg">
+                            <img src="images/facebook.png" alt="hero">
+                            <p>FACEBOOK: </p>
+                        </div>
+
+                        <p class="p1">Helados Splendid La Paz Bolivia </p>
+                    </div>
+                </div>
+                <div class="info">
+                    <p>2023 por: Universidad Católica Boliviana "San Pablo"</p>
+                </div>
+
+            </div>
         </footer>
+
         <script>
             AOS.init({
                 duration: 1000
             });
         </script>
 </body>
+
 </html>
